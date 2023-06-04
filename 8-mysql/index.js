c64c6ef86ca665a3c8cfd6c052dede4c8ad7ee83
vladimirmonteiro <vladimir.monteiro021@gmail.com>
2023-06-03 22:25:26 -0300

    three commit

--- 8-mysql/index.js
+++ 8-mysql/index.js
@@ -15,6 +15,93 @@ app.set('view engine', 'handlebars')
 
 
 
+
+
+app.get('/books', (req, res)=> {
+    const sql = "SELECT * FROM books"
+    conn.query(sql, function(err, data){
+        if(err){
+            console.log(err)
+            return
+        }
+
+        const books = data
+        console.log(books)
+
+        res.render('books', {books})
+
+    })
+})
+
+app.get('/book/:id', (req,res) => {
+    const id = req.params.id
+
+    const sql = `SELECT * FROM books WHERE id = ${id}`
+
+    conn.query(sql, function(err, data){
+
+        if(err){
+            console.log(err)
+            return
+        }
+
+        const book = data[0]
+
+        res.render('book', { book })
+    })
+})
+
+app.get('/books/edit/:id', (req, res)=> {
+    const id = req.params.id
+
+    const sql = `SELECT * FROM books WHERE id = ${id}`
+
+    conn.query(sql, function(err, data){
+        if(err){
+            console.log(err)
+            return
+        }
+
+        const book = data[0]
+        console.log(data)
+
+        res.render('bookedit', {book})
+    })
+})
+
+
+app.post('/book/updatebook', (req, res)=> {
+    const {id, title, pages} = req.body
+
+    const sql = `UPDATE books SET titulo = '${title}', paginas = '${pages}' WHERE id = ${id}`
+
+    conn.query(sql, function(err){
+        if(err){
+            console.log(err)
+            return
+        }
+
+        res.redirect('/books')
+    })
+})
+
+app.post('/books/insertbook', (req, res)=> {
+    const {title, pages} = req.body
+
+    const sql = `INSERT INTO books (titulo, paginas) VALUES ('${title}', '${pages}') `
+
+    conn.query(sql, function(err){
+        
+        if(err){
+            console.log(err)
+        }
+
+        res.redirect('/books')
+    })
+})
+
+
+
 app.get('/', (req, res)=> {
     res.render('home')
 })
